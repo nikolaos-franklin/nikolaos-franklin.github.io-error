eeadbc174a3d90537abe7d145ecba59a630dd8fd
nikolaos-franklin <katya.nikolaos17@gmail.com>
2021-09-04 20:43:48 +0300

    added full article page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,30 @@
 $(function(){
 
+    $('.recent-articles__slider').slick({
+      dots: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      infinite: true,
+      autoplay: true,
+      speed: 1000
+    });
+
+    $(".recent-articles__top-stars").each( function() {
+    var rating = $(this).attr("data-rating");
+    $(this).rateYo(
+        {
+            rating: rating,
+            rating: 4,
+            starWidth: "24px",
+            normalFill: "#E1E1E1",
+            ratedFill: "#06B25F",
+            spacing: "10px",
+            readOnly: true
+        }
+    );
+});
+
   $(".article__helpful-stars").each( function() {
     var rating = $(this).attr("data-rating");
     $(this).rateYo(
