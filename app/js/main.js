bc84a0e090f3ffb7d43a8f9aba68937f8d246b14
nikolaos-franklin <katya.nikolaos17@gmail.com>
2021-09-10 19:18:57 +0300

    added faq section

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,12 @@
 $(function(){
 
-  // Get the modal
-const popupLinks = document.querySelectorAll('.popup');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
+    $('.faq__spoiler-top').click(function(event){
+     $(this).toggleClass('active').next().slideToggle(300);
+   });
 
    $('.current__spollers-top').click(function(event){
      $(this).toggleClass('active').next().slideToggle(300);
    });
-
-
-  document.getElementById('get_file').onclick = function() {
-    document.getElementById('my_file').click();
-  };
 
 
     $('.recent-articles__slider').slick({
@@ -75,6 +47,7 @@ window.onclick = function(event) {
         }
     );
 });
+
 
   $(".article__top-stars").each( function() {
     var rating = $(this).attr("data-rating");
@@ -137,4 +110,34 @@ window.onclick = function(event) {
     spacing: "10px",
     readOnly: true
   });
+
+    // Get the modal
+const popupLinks = document.querySelectorAll('.popup');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+  document.getElementById('get_file').onclick = function() {
+    document.getElementById('my_file').click();
+  };
 });
